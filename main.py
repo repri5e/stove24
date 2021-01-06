29b8c41d09cbe3892f65770887174d9e7e2aa94a
IDCallName <73123314+IDCallName@users.noreply.github.com>
2021-01-06 20:57:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import os
 class Server (BaseHTTPRequestHandler):
   
   def do_GET (self):
+    self.send_response(200)
+    
     self.wfile.write(bytes("Hello world", "utf-8"))
+
+    self.end_headers()
    
 httpd = HTTPServer(("", int(os.environ['PORT'])), Server)
 httpd.serve_forever()
