f5a8544ac634ae9bda0fd9d822476bd570a66ef1
IDCallName <73123314+IDCallName@users.noreply.github.com>
2021-01-07 19:17:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask('__name__')
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return 'ккал'
 
 if __name__ == '__main__':
     app.run(host = "0.0.0.0", port = os.environ.get("PORT", 5000))
